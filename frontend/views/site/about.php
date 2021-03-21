7ad97379b27f1be935bade4efeb01021b879f30d
mustafayasar <mustafayasar@live.com>
2021-03-21 16:27:59 +0300

    frontend some edits

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -4,13 +4,13 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'Hakkında';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>Hakkında sayfası:</p>
 
     <code><?= __FILE__ ?></code>
 </div>
--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -8,14 +8,14 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
 
-$this->title = 'Contact';
+$this->title = 'İletişim';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-contact">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+        İletişim sayfası özelleştirilecek.
     </p>
 
     <div class="row">
